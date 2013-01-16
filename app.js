dcc7228e99c53a45887b93a2ac840c1d1f9bac36
feiyunruyue <wangliangfei4585@163.com>
2013-01-16 21:34:02 +0800

    chat demo

---
+++ app.js
@@ -0,0 +1,68 @@
+﻿var express = require('express')
+  , http = require('http')
+  , path = require('path');
+
+var app = express();
+var server = http.createServer(app);
+var WebSocketServer = require('ws').Server;
+var wss = new WebSocketServer({server : server});
+var colors = [ 'red', 'green', 'blue', 'magenta', 'purple', 'plum', 'orange' ];
+colors.sort(function(a,b) { return Math.random() > 0.5; } );
+var clients = [];
+
+wss.on('connection', function(ws){
+clients.push(ws);
+var userName = false;
+var userColor = false;
+  ws.on('message', function(msg){
+    if(!userName){
+        userName = msg;
+        userColor = colors.shift();
+        ws.send(JSON.stringify({ type:'color', data: userColor }));
+        console.log(userName + ' login');
+    }else{
+        console.log(userName + ' say: ' + msg);
+        var obj = {
+          time: (new Date()).getTime(),
+          text: msg,
+          author: userName,
+          color: userColor
+        };
+        var json = JSON.stringify({type:'message', data: obj});
+         for (var i=0; i < clients.length; i++) {
+             clients[i].send(json);
+         }
+    }
+  });
+  ws.on('close', function(){
+  if(userName !== false && userColor != false){
+    var index = clients.indexOf(ws);
+    clients.splice(index, 1);
+    colors.push(userColor);
+  }  
+  });
+  
+});
+
+app.configure(function(){
+  app.set('port', process.env.PORT || 3000);
+  app.set('views', __dirname + '/views');
+  app.use(express.favicon());
+  app.use(express.logger('dev'));
+  app.use(express.bodyParser());
+  app.use(express.methodOverride());
+  app.use(app.router);
+  app.use(express.static(path.join(__dirname, 'public')));
+});
+
+app.configure('development', function(){
+  app.use(express.errorHandler());
+});
+
+app.get('/', function(req, res){
+res.sendfile('views/chat.html');
+});
+
+server.listen(app.get('port'), function(){
+  console.log("Express server listening on port " + app.get('port'));
+});
---
+++ public/javascripts/browser.js
@@ -0,0 +1,41 @@
+﻿$(function () {
+    var content = $('#content');
+    var status = $('#status');
+    var input = $('#input');
+    var myColor = false;
+    var myName = false;
+    var connection = new WebSocket('ws://localhost:3000');
+
+    connection.onopen = function () {
+        status.text('Choose name:');
+    };
+    connection.onmessage = function (message) {
+      var json = JSON.parse(message.data);
+         if (json.type === 'color') { // first response from the server with user's color
+            myColor = json.data;
+            status.text(myName + ': ').css('color', myColor);
+        }else if (json.type === 'message') { // it's a single message
+            addMessage(json.data.author, json.data.text,
+                       json.data.color, new Date(json.data.time));
+        } 
+    };
+
+    input.keydown(function(e) {
+        if (e.keyCode === 13) {
+            var msg = $(this).val();
+            if (!msg)    return;
+            connection.send(msg);
+            $(this).val('');
+            if (myName === false) {
+                myName = msg;
+            }
+        }
+    });
+
+
+    function addMessage(author, message, color, dt) {
+        content.append('<p><span style="color:' + color + '">' + author + '</span> @ ' +
+             + dt.getHours() + ':'
+             + dt.getMinutes() + '----' + message + '</p>');
+    }
+});
